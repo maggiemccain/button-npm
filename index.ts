03df5f6f4186d0ca70705882ccf705ddbe9234d0
Maggie McCain <maggie.mccain@Macintosh-2.local>
2017-08-22 11:48:51 -0400

    exporting AmazingComponent

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
 export { AmazingModule } from './src/app/amazing.module';
 export { ButtonComponent } from './src/app/button/button.component';
+export { AmazingComponent } from './src/app/amazing.component';
